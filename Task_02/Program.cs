1c5c9c7704c95dfd6a3f4ea141d43a1aa281b970
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-22 18:54:18 +0700

    changing names of folders 2

---
+++ Task_02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please, input the first integer number!");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, input the second integer number!");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 == n2) {
+    Console.WriteLine("You inputed the equal numbers!");
+} else if (n1 > n2) {
+    Console.WriteLine($"The first number {n1} more then the second number {n2} !");
+} else {
+    Console.WriteLine($"The first number {n1} less then the second number {n2} !");
+}
