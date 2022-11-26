1e98c8162581c605b5d96035ab24ec7e3110c6b1
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-26 13:15:09 +0700

    adding of Task_07_GetOfTheirdDigit

---
+++ Task_07_GetOfTheirdDigit/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Please, input the integer number!");
+int number = Convert.ToInt32(Console.ReadLine());
+int numberAbs = Math.Abs(number);
+double degree = Math.Log10(d: numberAbs);
+
+if (degree < 2)
+{
+    Console.WriteLine("Inputing number has NO the theird digital!");
+}
+else
+{
+    int d = (int)Math.Pow(x: 10, y: (int)(degree) - 2);
+    int result = (numberAbs / d) % 10;
+    Console.WriteLine($"The theird digital of inputing number is {result}!");
+    }
