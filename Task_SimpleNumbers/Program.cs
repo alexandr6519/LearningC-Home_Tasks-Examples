27b60c1e45de57122d527f165f7a6f560f07bf04
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-26 13:20:15 +0700

    adding of Task_SimpleNumbers

---
+++ Task_SimpleNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Please, input the integer number!");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"The list of simple numbers between 2 and {0} is: ", n);
+
+for (int i = 2; i <= n; i++)
+{
+    bool isSimple = true;
+    for (int j = 2; j < i; j++)
+    {
+        if (i % j == 0)
+        {
+            isSimple = false;
+            break;
+        }
+    }
+    if (isSimple) Console.Write(i + " ");
+}
