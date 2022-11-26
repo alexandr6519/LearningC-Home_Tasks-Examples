3486b180125523c9968ecd8242f51abc9528344c
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-26 13:19:38 +0700

    adding of Task_MaxNumber

---
+++ Task_MaxNumber/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Please, input the integer number!");
+int n1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Please, input jne more the integer number!");
+int n2 = Convert.ToInt32(Console.ReadLine());
+int max = (n1 > n2) ? n1 : n2;
+Console.WriteLine("The maximum number from {0} and {1} is {2}!", n1, n2, max);
