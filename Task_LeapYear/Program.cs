a2dc9ed568893dee05e2d592f60dd6f9cf123b68
Ikonnikov Aleksandr <alexandr6519@mail.ru>
2022-11-26 13:18:21 +0700

    adding of Task_LeapYear

---
+++ Task_LeapYear/Program.cs
@@ -0,0 +1,3 @@
+﻿int numberYear = new Random().Next(1900, 2100);
+if ((numberYear - 1896) % 4 == 0) Console.WriteLine("This year {0} is leap! ", numberYear);
+else Console.WriteLine("This year {0} is NOT leap! ", numberYear);
